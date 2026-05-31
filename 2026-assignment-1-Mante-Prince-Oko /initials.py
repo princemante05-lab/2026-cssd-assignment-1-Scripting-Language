1dc173de67fb92a69fbb55900bc31e33d61ab553
princemante05-lab <princemante05@gmail.com>
2026-05-31 17:01:34 +0000

    Create initials.py

---
+++ 2026-assignment-1-Mante-Prince-Oko /initials.py
@@ -0,0 +1,14 @@
+#File initials.py
+#Description: Print out my initials,
+#Assignments: 1
+#
+#Name: Mante Prince Oko
+#STUDENT ID: 2425404775
+#Email: 2425404775@live.gctu.edu.gh
+#Grader: Augustus
+#
+# On my honor, Mante Prince Oko, this programming assignment is my own work
+# and I have not provided rhis code to any other student.
+
+
+def main()
