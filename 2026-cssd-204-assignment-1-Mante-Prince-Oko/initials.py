8d2b5316ebecff0156790a39090d038ef936e3fb
princemante05-lab <princemante05@gmail.com>
2026-05-31 17:54:44 +0000

    Create initials.py

---
+++ 2026-cssd-204-assignment-1-Mante-Prince-Oko/initials.py
@@ -0,0 +1,36 @@
+#File initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Mante PRince Oko
+#STUDENT ID: 2425404775
+#Email: 2425404775@live.gctu.edu.gh
+#Grader: Augustus
+#
+# On my honor, Mante Prince Oko, this programming assignment is my own work
+# amd I have not provided this code to any other student.
+
+
+def main():
+  #Code tom print out my initials ETO
+  
+  print("print out my initials\n\n")
+
+  print("...ETO\n\n")
+
+print("...EEEEEEEEEEEEEEE...........TTTTTTTTTTTTTTTTTT..........OOOOOOOOOOOOOOOO...")
+print("...EEEEEEEEEEEEEEE...........TTTTTTTTTTTTTTTTTT..........O.O............O.O.....")
+print("...EEEEEEE........................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEE........................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEEEEEEEEEE................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEEEEEEEEEE................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEE........................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEE........................TTTTTTTT...............O.O............O.O.....")
+print("...EEEEEEEEEEEEEEE....**..........TTTTTTTT.......**......O.O............O.O...**")
+print("...EEEEEEEEEEEEEEE....**..........TTTTTTTT.......**......OOOOOOOOOOOOOOOO....**\n\n")
+
+
+
+
+
+main()
